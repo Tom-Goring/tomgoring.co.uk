fca2633c30174fdffd0f61f06b9200e4b6dfc8c6
Tom Goring <tgoring97@gmail.com>
2020-06-08 16:57:06 +0100

    Add delete, find_all, and update routes

--- src/todo/model.rs
+++ src/todo/model.rs
@@ -33,7 +33,6 @@ impl Responder for Todo {
 
 impl Todo {
     pub async fn create(todo: TodoRequest, pool: &PgPool) -> Result<Todo> {
-        println!("Creating new todo");
         let mut tx = pool.begin().await?;
         let todo = sqlx::query(
             "INSERT INTO todos (description, done) VALUES ($1, $2) RETURNING id, description, done",
@@ -52,12 +51,30 @@ impl Todo {
         Ok(todo)
     }
 
+    pub async fn find_all(pool: &PgPool) -> Result<Vec<Todo>> {
+        let mut todos = vec![];
+
+        let recs = sqlx::query!(r#" SELECT * FROM todos ORDER BY id"#)
+            .fetch_all(pool)
+            .await?;
+
+        for rec in recs {
+            todos.push(Todo {
+                id: rec.id,
+                description: rec.description,
+                done: rec.done,
+            });
+        }
+
+        Ok(todos)
+    }
+
     pub async fn find_by_id(id: i32, pool: &PgPool) -> Result<Todo> {
         let rec = sqlx::query!(
             r#"
                 SELECT * FROM todos WHERE id = $1
             "#,
-            id
+            id,
         )
         .fetch_one(&*pool)
         .await?;
@@ -67,5 +84,36 @@ impl Todo {
             description: rec.description,
             done: rec.done,
         })
+    }
+
+    pub async fn delete(id: i32, pool: &PgPool) -> Result<i32> {
+        let mut tx = pool.begin().await?;
+        let deleted = sqlx::query("DELETE FROM todos WHERE id = $1")
+            .bind(id)
+            .execute(&mut tx)
+            .await?;
+
+        tx.commit().await?;
+        Ok(deleted as i32)
+    }
+
+    pub async fn update(id: i32, todo: TodoRequest, pool: &PgPool) -> Result<Todo> {
+        let mut tx = pool.begin().await.unwrap();
+        let todo = sqlx::query("UPDATE todos SET description = $1, done = $2 WHERE id = $3 RETURNING id, description, done")
+            .bind(&todo.description)
+            .bind(todo.done)
+            .bind(id)
+            .map(|row: PgRow| {
+                Todo {
+                    id: row.get(0),
+                    description: row.get(1),
+                    done: row.get(2)
+                }
+            })
+            .fetch_one(&mut tx)
+            .await?;
+
+        tx.commit().await.unwrap();
+        Ok(todo)
     }
 }
--- src/todo/routes.rs
+++ src/todo/routes.rs
@@ -5,7 +5,11 @@ use crate::todo::{Todo, TodoRequest};
 
 #[get("/todos")]
 async fn find_all(db_pool: web::Data<PgPool>) -> impl Responder {
-    "get todos"
+    let result = Todo::find_all(db_pool.get_ref()).await;
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        _ => HttpResponse::BadRequest().body("Error getting todos from database"),
+    }
 }
 
 #[get("/todo/{id}")]
@@ -26,8 +30,32 @@ async fn create(todo: web::Json<TodoRequest>, db_pool: web::Data<PgPool>) -> imp
     }
 }
 
+#[delete("/todo/{id}")]
+async fn delete(id: web::Path<i32>, db_pool: web::Data<PgPool>) -> impl Responder {
+    let result = Todo::delete(id.into_inner(), db_pool.get_ref()).await;
+    match result {
+        Ok(deleted) => HttpResponse::Ok().json(deleted),
+        _ => HttpResponse::BadRequest().body("Error deleting resource"),
+    }
+}
+
+#[put("/todo/{id}")]
+async fn update(
+    id: web::Path<i32>,
+    todo: web::Json<TodoRequest>,
+    db_pool: web::Data<PgPool>,
+) -> impl Responder {
+    let result = Todo::update(id.into_inner(), todo.into_inner(), db_pool.get_ref()).await;
+    match result {
+        Ok(todo) => HttpResponse::Ok().json(todo),
+        _ => HttpResponse::BadRequest().body("Error updating resource"),
+    }
+}
+
 pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(create);
     cfg.service(find_all);
     cfg.service(find);
+    cfg.service(create);
+    cfg.service(delete);
+    cfg.service(update);
 }
